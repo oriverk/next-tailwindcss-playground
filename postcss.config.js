ba49d91bc054dd34f46772120aab11d87ad2a9bc
oriverk <oriveryk@gmail.com>
2021-01-27 07:11:03 +0000

    modify postcss config to change behaivor in dev and in build

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,20 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
-    '@fullhuman/postcss-purgecss': {
-      content: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    },
-    'postcss-custom-properties': {},
-    'postcss-preset-env': { stage: 1 },
-    cssnano: { preset: 'default' },
+    ...(process.env.NODE_ENV === 'production') && {
+      autoprefixer: {},
+      '@fullhuman/postcss-purgecss': {
+        content: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      },
+      'postcss-custom-properties': {},
+      'postcss-preset-env': { stage: 1 },
+      cssnano: { preset: 'default' },
+    }
   },
 }
+
+// https://github.com/vercel/next.js/issues/10117#issuecomment-575263084
 
 // If you want to use other PostCSS plugins, see the following:
 // https://tailwindcss.com/docs/using-with-preprocessors
