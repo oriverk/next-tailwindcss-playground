f0229fdbe6c85943d30fa9c198a249b1c1927943
oriverk <oriveryk@gmail.com>
2021-01-27 07:11:45 +0000

    modify to import cssfiles in _document.tsx through raw-loader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,5 @@
 import { AppProps } from 'next/app'
 
-import '../style/bundle.css'
-
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,39 @@
-import Document, { DocumentContext } from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+
+// @ts-ignore
+import devCss from '!raw-loader!../style/global.css'
+// @ts-ignore
+import buildCss from '!raw-loader!../style/bundle.min.css'
+const cssFile = process.env.NODE_ENV === 'development' ? devCss : buildCss
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
+    const page = ctx.renderPage((App) => (props) => <App {...props} />)
+    const initialProps: any = await Document.getInitialProps(ctx)
+    return {
+      ...page,
+      styles: [
+        ...initialProps.styles,
+        <style
+          key='custom'
+          dangerouslySetInnerHTML={{
+            __html: cssFile
+          }}
+        />
+      ]
+    }
+  }
 
-    return initialProps
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
 
